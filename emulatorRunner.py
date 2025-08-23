18c4feee30adc282eb7f47c2db34525fb0e690c3
Arnaud Delorme <arnodelorme@gmail.com>
2025-08-22 21:10:27 -0700

    adding LSL mac lib

--- emulatorRunner.py
+++ emulatorRunner.py
@@ -1,9 +1,20 @@
-from EmulatorCOM import EmulatorCOM
-from AxonCOM import State
-import serial.tools.list_ports as port_list
 import sys
 import os
-os.environ["PYLSL_LIB"] = r"C:\\Users\\Arnaud Delorme\\Desktop\\lslEmulator\\liblsl-1.16.2-Win_amd64\\lib\\lsl.lib"
+import serial.tools.list_ports as port_list
+
+# Set PYLSL_LIB environment variable BEFORE importing any modules that use pylsl
+if sys.platform == "win32":
+    os.environ["PYLSL_LIB"] = r"C:\\Users\\Arnaud Delorme\\Desktop\\lslEmulator\\liblsl-1.16.2-Win_amd64\\lib\\lsl.lib"
+elif sys.platform == "darwin":
+    os.environ["PYLSL_LIB"] = r"/Users/arno/Python/lslEmulator/liblsl-1.16.2-OSX_amd64/lib/liblsl.dylib"
+else:
+    print("Unsupported platform")
+    exit()
+
+# Now import modules that depend on pylsl
+from EmulatorCOM import EmulatorCOM
+from AxonCOM import State
+
 streamingStarted = False
 emmy = EmulatorCOM()
 
