d4ff72060a62569ba9455f88918aff58880dffe6
Arnaud Delorme <arnodelorme@gmail.com>
2025-08-22 18:44:42 -0700

    making it work for windows

--- emulatorRunner.py
+++ emulatorRunner.py
@@ -2,6 +2,8 @@ from EmulatorCOM import EmulatorCOM
 from AxonCOM import State
 import serial.tools.list_ports as port_list
 import sys
+import os
+os.environ["PYLSL_LIB"] = r"C:\\Users\\Arnaud Delorme\\Desktop\\lslEmulator\\liblsl-1.16.2-Win_amd64\\lib\\lsl.lib"
 streamingStarted = False
 emmy = EmulatorCOM()
 
